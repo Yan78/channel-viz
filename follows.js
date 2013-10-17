42aa58b734632f81068f75c94309406fe1f79fb8
Yan78 <njcs2006@gmail.com>
2013-10-17 13:17:43 +0300

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= 'BaVVvINO2yZX5WX96pc6iu9wG7vXaAjVJlLUfWdaW31y7Dab', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [2019323701], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Xively Arduino Room Monitor', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
